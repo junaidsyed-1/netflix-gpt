01a9465c337f60a82e05ab0bd4da10bcc7bf308d
junaid <jaanusd79@gmail.com>
2023-12-21 17:02:48 +0530

    Completed auth with firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,52 @@
-import { useState } from "react"
+import { useRef, useState } from "react";
+import { checkValidData } from "../utils/validate";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
 
-    const [isSignInForm, setisSignInForm] = useState(true)
 
-    const handleSubmit = () => {
+    const [isSignInForm, setisSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const email = useRef(null);
+    const password = useRef(null);
+
+    const handleClick = () => {
+        const message = checkValidData(email.current.value, password.current.value);
+        console.log(message);
+        setErrorMessage(message);
+        if (message) return;
+
+        // Sign up/ Sign In Logic.
+        if (!isSignInForm) {
+            // Sign up
+            createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + "-" + errorMessage);
+                });
+        } else {
+            // Sign In
+            signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setErrorMessage(errorCode + "-" + errorMessage);
+                });
+        };
+    }
+
+    const handleToggleSubmit = () => {
         setisSignInForm(!isSignInForm);
     }
 
@@ -25,10 +67,17 @@ const Login = () => {
                 className="p-16 w-3/12 bg-black absolute text-white opacity-95 rounded-md">
                 <h1 className="text-3xl font-bold mb-5 mt-2">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
                 {!isSignInForm && <input type="text" placeholder="Full Name" className="py-3 px-4 my-3 rounded-lg w-full bg-[#8c8c8c] text-white" />}
-                <input type="email" placeholder="Email Address" className="py-3 px-4 my-3 rounded-lg w-full bg-[#8c8c8c] text-white" />
-                <input type="password" placeholder="Password" className="py-3 px-4 my-3 rounded-lg w-full bg-[#8c8c8c] text-white" />
-                <button className="bg-red-600 rounded-lg w-full py-2 my-2">{isSignInForm ? "Sign In" : "Sign Up"}</button>
-                <p className="mt-7"><span className="text-[#8c8c8c]">{isSignInForm ? "New to NetFlix?" : "Already a user?"}</span> <span onClick={handleSubmit} className="hover:underline cursor-pointer"> {isSignInForm ? "Sign up Now." : "Sign In"} </span></p>
+                <input
+                    ref={email}
+                    type="email" placeholder="Email Address" className="py-3 px-4 my-3 rounded-lg w-full bg-[#8c8c8c] text-white" />
+                <input
+                    ref={password}
+                    type="password" placeholder="Password" className="py-3 px-4 my-3 rounded-lg w-full bg-[#8c8c8c] text-white" />
+                <p className="text-red-700 font-bold">{errorMessage}</p>
+                <button
+                    onClick={handleClick}
+                    className="bg-red-600 rounded-lg w-full py-2 my-2">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+                <p className="mt-7"><span className="text-[#8c8c8c]">{isSignInForm ? "New to NetFlix?" : "Already a user?"}</span> <span onClick={handleToggleSubmit} className="hover:underline cursor-pointer"> {isSignInForm ? "Sign up Now." : "Sign In"} </span></p>
             </form>
         </div>
     )
